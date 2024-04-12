c29e7116e8b7f8c999aeacabe810c83dc69026da
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-04-12 13:55:57 -0300

    YXE - Domain Cors

--- app/server.ts
+++ app/server.ts
@@ -1,8 +1,20 @@
 
 import express from "express"
 import { router } from "./router";
+import cors from 'cors';
 
 const app = express();
+
+const allowedOrigins = [
+    'https://agenciayxeapi.vercel.app',
+    'https://yxe.com.br',
+    'https://www.yxe.com.br',
+];
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+
+app.use(cors(options));
 
 app.use(express.json());
 
