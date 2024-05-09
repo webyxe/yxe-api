3be9ae22a94ee1b0e2562bd15d808114914b76ec
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2024-05-09 14:12:42 -0300

    Ajuste - Prisma

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -190,6 +190,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -191,6 +191,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
